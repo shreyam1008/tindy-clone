ccadd59c6528be1a3e119eafc6df3c3d2e2abb42
Shreyam Adhikari <shreyam1008@gmail.com>
2020-10-25 08:05:24 +0545

    bottm buttons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from "react";
 import Header from "./Header";
+import SwipeButtons from "./SwipeButtons";
 import TinderCards from "./TinderCards";
 
 const App = () => {
   return (
     <div className="app">
       <Header />
-      {/* tindercards */}
       <TinderCards />
-
-      {/* swipe buttons */}
-    {/* <SwipeButtons /> */}
-
+      <SwipeButtons />
     </div>
   );
 };
